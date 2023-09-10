f7611670010e22b26f0c697b0aee4f67d3bc83a3
araf821 <araf.ahmed200@gmail.com>
2023-09-10 17:00:00 -0400

    initial commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface LandingPageProps {}
+
+const LandingPage: FC<LandingPageProps> = ({}) => {
+  return <div>Landing Page</div>;
+};
+
+export default LandingPage;
